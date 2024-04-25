345213509a5caa4b1a0146d0e91dd0ceb73525e4
Rishikesh <rishikesh.mmf@gmail.com>
2024-04-25 17:19:46 +0530

    latest commit

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ var app = express();
 app.set("view engine", "ejs");
 
 // public folder to store assets
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "../public"));
 
 // routes for app
 app.get("/", function (req, res) {
